c0a8941d711fbdb2cfd06d7ef27a72d182669a77
slovo88 <prmoore45@gmail.com>
2020-04-12 13:48:39 -0400

    feat: add gh-pages deploy capability

---
+++ build/precache-manifest.89ae66704309a409242457eae67dcbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa69cf48e73a18ecdacbe77761abab2e",
+    "url": "/phase10/index.html"
+  },
+  {
+    "revision": "1f16549f714cb3fa98bb",
+    "url": "/phase10/static/css/main.c444ef2f.chunk.css"
+  },
+  {
+    "revision": "2d0b15cef7297bc98348",
+    "url": "/phase10/static/js/2.c1b3acc8.chunk.js"
+  },
+  {
+    "revision": "a671c0bdee409a39c875ed03e2957619",
+    "url": "/phase10/static/js/2.c1b3acc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f16549f714cb3fa98bb",
+    "url": "/phase10/static/js/main.cc91c9e9.chunk.js"
+  },
+  {
+    "revision": "3701017f2bd50aa61469",
+    "url": "/phase10/static/js/runtime-main.b3a79d1a.js"
+  }
+]);
